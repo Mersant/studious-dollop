67990a1065159e3d2cb2294e2d1c074aeb29525d
Mersant <dlowry99@outlook.com>
2022-08-16 10:36:56 -0400

    Added placeholder function for edamam API call

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+
+// searchIngredients accepts a string of ingredients in plain english, i.e. "A can of beans an 16 ounces of coffee" or "coffee and a croissant" and nutritional information via the edamam API such as Calories, fat, alcohol, etc. Example call: searchIngredients("Pasta and sauce");
+function searchIngredients(ingredients) {
+    $.ajax({
+        type: "GET",
+        url: "https://api.edamam.com/api/food-database/v2/parser?app_id=08017788&app_key=19280c7005f2a1136addc35fbe298419&ingr=" + ingredients + "&nutrition-type=cooking",
+        dataType: "json",
+        success: function (result, status, xhr) {
+            // Loop through each ingredient found within the "ingredients" parameter.
+            for(var i=0; i<=result["parsed"].length; i++) {
+                /* -- Copy nutrition data to HTML page */
+            }
+            
+        },
+        error: function (xhr, status, error) {
+            alert("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText);
+        }
+    })
+}
+
