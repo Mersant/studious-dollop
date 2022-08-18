91d724551b5b5a3052b417173f5aecc78bc5e140
omarfarah22 <cronogy@gmail.com>
2022-08-18 13:57:36 -0400

    Added weather dashboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,3 +56,89 @@ function updateNutrition(ingredients) {
         }
     })
 }
+
+//---------------------------------------Weather------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//Gets the clients location info
+function getLocationInfo(lat, lon){
+    var requestUrl = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=02024b9f7001696a944662ca0b291629`
+    fetch(requestUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+            cityName = data[0].name;
+            getWeather(cityName)
+        })
+}
+//Api used to show location
+function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } 
+  }
+  //Api used to get the lat and long of the client
+function showPosition(position) {
+    var lat = position.coords.latitude 
+    console.log(lat)
+    var lon = position.coords.longitude
+    console.log(lon)
+    getLocationInfo(lat, lon)
+  }
+
+  console.log(getLocation())
+
+//Gets the weather data of the city passed in by the function
+function getWeather(city){
+
+    var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&exclude=hourly,daily&appid=02024b9f7001696a944662ca0b291629&units=imperial&cnt=1`
+
+
+        fetch(requestUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+            var temp = `${data.list[0].main.temp}°F`
+            var cityTemp = `Temprature ${data.list[0].main.temp}°F`;
+            var feelsLike = `Feels like ${data.list[0].main.feels_like}°F`;
+            var tempMin = `Low ${data.list[0].main.temp_min}°F`;
+            var tempMax = `High ${data.list[0].main.temp_max}°F`;
+            var humidity = `Humidity ${data.list[0].main.humidity}%`;
+            var cityName = `City ${data.city.name}`;
+            var countryName = `Country ${data.city.country}`;
+            var population = `Population ${data.city.population}`
+            var weather = `Weather ${data.list[0].weather[0].main}`;
+            var weatherData = [cityName, countryName, population, weather, cityTemp, tempMin, tempMax, humidity, feelsLike]
+
+            $("#weather").html("");
+            var grand = document.querySelector("#grand")
+            grand.innerHTML = temp
+            var cityNameDiv = document.querySelector("#cityName");
+            cityNameDiv.innerHTML = cityName;
+            var countryNameDiv = document.querySelector("#countryName")
+            countryNameDiv.innerHTML = countryName;
+            var cityTempDiv = document.querySelector("#cityTemp");
+            var cityTempDivSpan = document.querySelector(".ms-1");
+            cityTempDivSpan.innerHTML = cityTemp
+            var feelsLikeDiv = document.querySelector("#feelsLike");
+            var feelsLikeDivSpan = feelsLikeDiv.querySelector(".ms-1");
+            feelsLikeDivSpan.innerHTML = feelsLike;
+            var tempMinDiv = document.querySelector("#tempMin");
+            var tempMinDivSpan = tempMinDiv.querySelector(".ms-1")
+            tempMinDivSpan.innerHTML = tempMin
+            var tempMaxDiv = document.querySelector("#tempMax")
+            var tempMaxDivSpan = tempMaxDiv.querySelector(".ms-1");
+            tempMaxDivSpan.innerHTML = tempMax;
+            var humidityDiv = document.querySelector("#humidity");
+            var humidityDivSpan = humidityDiv.querySelector(".ms-1");
+            humidityDivSpan.innerHTML = humidity
+
+            
+        });
+}
+
+//----------------------------------------------ToDoList----------------------------------------------------------------------------
+
+getLocationInfo()
+
+
